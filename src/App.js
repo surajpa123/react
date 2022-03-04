1c10be4e893d9d9e2fdf1706f86fb1681bbc8428
surajpa123 <77233053+surajpa123@users.noreply.github.com>
2022-03-04 23:40:08 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
-  const os = ["Android","Blackberry","iPhone","Windows Phone"];
+  const [counter,setCounter] = React.useState(0)
+  const handelincrement = (value) => {
+   
+   setCounter(counter + value);
+   console.log(counter)
+   
+  }
+  const double = (value)=>{
+    if(counter == 0){
+      setCounter(1 * value)
+    }else{
 
+      setCounter(counter * value)
+    }
+  }
   return (
-   <div>
-  <h1>Hello{os[0]}</h1>
+   <div className='App'>
+  <h1>Counter App</h1>
+  <h2>{counter}</h2>
+  <button onClick={() =>handelincrement(1)}>Increment</button>
+  <button onClick= {() =>handelincrement(-1)} >Decrement</button>
+  <button onClick={() => double(2)}>Double</button>
    </div>
   );
 }
