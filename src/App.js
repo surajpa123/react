aee9d642b3c7c66cdad17a58591b025ce214b16c
surajpa123 <77233053+surajpa123@users.noreply.github.com>
2022-03-04 23:54:31 +0530

    fixed some errors

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import { useState } from 'react';
 function App() {
   const [counter,setCounter] = React.useState(0)
   const handelincrement = (value) => {
-   
    setCounter(counter + value);
    console.log(counter)
-   
+  }
+  const handeldec = (value) =>{
+    if(counter <= 0){
+      return
+    // setCounter(1 + value)
+    }else{
+      setCounter(counter + value)
+    }
   }
   const double = (value)=>{
     if(counter == 0){
@@ -24,7 +30,7 @@ function App() {
   <h1>Counter App</h1>
   <h2>{counter}</h2>
   <button onClick={() =>handelincrement(1)}>Increment</button>
-  <button onClick= {() =>handelincrement(-1)} >Decrement</button>
+  <button onClick= {() =>handeldec(-1)} >Decrement</button>
   <button onClick={() => double(2)}>Double</button>
    </div>
   );
